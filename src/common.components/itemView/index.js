cdf05501286ffad43f9fcbae153d1c759e1f5e1e
ganjahnavarro <ganjahnavarro@gmail.com>
2020-05-23 13:13:58 +0800

    updated item view to display real data

--- src/common.components/itemView/index.js
+++ src/common.components/itemView/index.js
@@ -1,23 +1,54 @@
 import React from "react";
+import { connect } from "react-redux";
 import { ItemList, OrderName } from "./components"
+
+import { products } from "../../utils/values.products";
 import { orderDetails } from "../../utils/dummy.array";
 
 
-const ItemView = () => (
-    <div>
-        {orderDetails
-            .sort((a, b) => b.quantity - a.quantity)
-            .map(item => ( 
-            <ItemList
-                key={item.id}
-                wrap
-                multipleLine align="top" 
-                extra={item.quantity}           
-            >
-                <OrderName>{item.productName}</OrderName>
-            </ItemList>
-        ))}
-    </div>    
-);
+const ItemView = ({ orders }) => {
+    const countsMap = new Map()
+    orders.forEach(order => {
+        order.orders.forEach(item => {
+            const currentCount = countsMap.get(item.productId);
+            countsMap.set(item.productId, currentCount ? currentCount + item.quantity : item.quantity);
+        })
+    });
 
-export default ItemView;
+    const itemsComponent = [];
+
+    for (const [key, value] of countsMap.entries()) {
+        const productName = getProductName(key);
+        itemsComponent.push(
+          <ItemList
+              key={key}
+              wrap multipleLine align="top"
+              extra={value}>
+              <OrderName>{productName}</OrderName>
+          </ItemList>
+        )
+    }
+
+    return <div>
+        {itemsComponent}
+    </div>;
+};
+
+const getProductName = (productId) => {
+    let productName;
+    products.forEach((product) => {
+        product.items.forEach((item) => {
+            if (item.id === productId) {
+                productName = item.name;
+            }
+        });
+    })
+    return productName;
+};
+
+const mapStateToProps = (state) => {
+    const { orders } = state.orders;
+    return { orders };
+};
+
+export default connect(mapStateToProps)(ItemView);
